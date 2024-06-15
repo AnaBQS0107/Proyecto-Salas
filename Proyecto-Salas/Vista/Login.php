ec9efbbb5ed88332a5a8766329f270bb7d95823c
Ana <anabqs0107@gmail.com>
2024-06-15 09:03:45 -0600

    ActualizacionLogin

--- Proyecto-Salas/Vista/Login.php
+++ Proyecto-Salas/Vista/Login.php
@@ -15,7 +15,7 @@
     <!-- Style -->
     <link rel="stylesheet" href="../Css/style.css">
 
-    <title>Login #7</title>
+    <title>Login -- Sistema de Ventas</title>
   </head>
   <body>
   
@@ -31,44 +31,31 @@
           <div class="row justify-content-center">
             <div class="col-md-8">
               <div class="mb-4">
-              <h3>Sign In</h3>
-              <p class="mb-4">Lorem ipsum dolor sit amet elit. Sapiente sit aut eos consectetur adipisicing.</p>
+             <center><h3>Inicio de sesión</h3></center> 
+              <p class="mb-4">Inicia sesión y navega por nuestra gran variedad de productos.</p>
             </div>
             <form action="#" method="post">
               <div class="form-group first">
-                <label for="username">Username</label>
+                <label for="username">Usuario</label>
                 <input type="text" class="form-control" id="username">
 
               </div>
               <div class="form-group last mb-4">
-                <label for="password">Password</label>
+                <label for="password">Contraseña</label>
                 <input type="password" class="form-control" id="password">
                 
               </div>
               
               <div class="d-flex mb-5 align-items-center">
-                <label class="control control--checkbox mb-0"><span class="caption">Remember me</span>
-                  <input type="checkbox" checked="checked"/>
-                  <div class="control__indicator"></div>
-                </label>
-                <span class="ml-auto"><a href="#" class="forgot-pass">Forgot Password</a></span> 
+      
+ 
               </div>
 
-              <input type="submit" value="Log In" class="btn btn-block btn-primary">
+              <input type="submit" value="Iniciar Sesión" class="btn btn-block btn-primary">
 
-              <span class="d-block text-left my-4 text-muted">&mdash; or login with &mdash;</span>
+
               
-              <div class="social-login">
-                <a href="#" class="facebook">
-                  <span class="icon-facebook mr-3"></span> 
-                </a>
-                <a href="#" class="twitter">
-                  <span class="icon-twitter mr-3"></span> 
-                </a>
-                <a href="#" class="google">
-                  <span class="icon-google mr-3"></span> 
-                </a>
-              </div>
+
             </form>
             </div>
           </div>
